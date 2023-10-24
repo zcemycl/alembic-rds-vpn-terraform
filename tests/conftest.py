47d3f0186153a41866245ddb1b7c497f4a830add
zcemycl <lyc010197@gmail.com>
2023-10-24 23:03:46 +0100

    add link test

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ from sqlalchemy.engine import Engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.sql import insert
 
+import example_package.dataclasses.orm as d
 from app.database import get_session
 from app.main import app
 from example_package.dataclasses import metadata, person
@@ -32,10 +33,53 @@ def get_engine() -> Engine:
     engine.dispose()
 
 
+@pytest.fixture(autouse=True)
+def reset(get_engine: Engine):
+    engine = get_engine
+    metadata.drop_all(bind=engine)
+    metadata.create_all(bind=engine)
+
+
+@pytest.fixture(scope="session")
+def get_engine2() -> Engine:
+    db_url = "postgresql://postgres:postgres@localhost/postgres"
+    engine = create_engine(db_url)
+
+    d.metadata.drop_all(bind=engine)
+    d.metadata.create_all(bind=engine)
+
+    with open(Path("tests/test_data/base-persons.json"), "r") as f:
+        jsons = json.load(f)
+
+    stmt = insert(person).values(jsons)
+    with engine.begin() as conn:
+        _ = conn.execute(stmt)
+
+    yield engine
+    engine.dispose()
+
+
+@pytest.fixture(autouse=True)
+def reset2(get_engine2: Engine):
+    engine = get_engine2
+    d.metadata.drop_all(bind=engine)
+    d.metadata.create_all(bind=engine)
+
+
 @pytest.fixture(scope="function", autouse=True)
 def test_session(get_engine: Engine) -> Session:
     session = sessionmaker(
         bind=get_engine, expire_on_commit=False, class_=Session
+    )
+    with session() as session:
+        yield session
+        session.close()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def test_session2(get_engine2: Engine) -> Session:
+    session = sessionmaker(
+        bind=get_engine2, expire_on_commit=False, class_=Session
     )
     with session() as session:
         yield session
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
 
-from sqlalchemy.sql import insert
+from sqlalchemy.sql import insert, select
 
+import example_package.dataclasses.orm as d
 from example_package.dataclasses import person
 
 
@@ -15,3 +16,28 @@ def test_db(get_engine):
     stmt = insert(person).values(jsons)
     with engine.begin() as conn:
         _ = conn.execute(stmt)
+
+
+def test_orm_link(test_session2):
+    p1 = d.person(firstname="1", lastname="1", others={}, role="developer")
+    s1 = d.skill(name="python", persons=[p1])
+    p2 = d.person(
+        firstname="2",
+        lastname="2",
+        others={},
+        role="developer",
+        parent_friendships=[p1],
+    )
+    test_session2.add_all([p1, s1, p2])
+    test_session2.commit()
+
+    p1_ = test_session2.execute(
+        select(d.person).where(d.person.id == p1.id)
+    ).scalar()
+
+    assert s1.id == p1_.skills[0].id
+    assert p1_.parent_friendships == []
+    assert p1_.child_friendships[0].id == p2.id
+    assert p1.child_friendships[0].id == p2.id
+    assert p2.child_friendships == []
+    assert p2.parent_friendships == [p1]
