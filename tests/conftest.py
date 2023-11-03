0cee4a4b7d31a693dfb7bff8bf186561d365b812
zcemycl <lyc010197@gmail.com>
2023-11-03 20:23:04 +0000

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,27 +15,12 @@ import example_package.dataclasses.orm as d
 from app.database import get_async_session
 from app.main import app
 
-# from example_package.dataclasses import metadata
-
 
 @pytest.fixture(scope="session")
 def event_loop() -> asyncio.AbstractEventLoop:
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
-
-
-# @pytest_asyncio.fixture(autouse=True)
-# async def get_engine():
-#     db_url = "postgresql+asyncpg://postgres:postgres@localhost/postgres"
-#     engine = create_async_engine(db_url)
-
-#     async with engine.begin() as conn:
-#         await conn.run_sync(metadata.drop_all)
-#         await conn.run_sync(metadata.create_all)
-
-#         yield conn
-#         engine.sync_engine.dispose()
 
 
 @pytest.fixture(autouse=True)
